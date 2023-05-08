6762eacce35714d61bdc2220629853cc260350c1
SashaSasha707 <askliar0707@gmail.com>
2023-05-08 09:49:49 +0300

    add code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,70 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+/*Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+Реалізація делегування на ul.gallery і отримання url великого зображення.
+Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+*/
+
+/*Алгоритм
+1. Отримати елемент gallery
+2. Створити зміну де буде зберігатись розмітка зображень.
+3. За допомогою innerHTML додати galleryMarkup до galleryContainer
+4. Додаємо обробник подій при кліку на зображення, щоб відкривалась велика картинка.
+5. Функція забороняє завантажння зображення (preventDefault). Застосовується бібліотека basicLightbox.
+6. У функцію також додаємо обробник подій на вікно - закриття через Escape.
+*/
+
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+                        .map((item) => 
+                            `<li class="gallery__item">
+                                <a class="gallery__link" href="${item.original}">
+                                    <img
+                                        class="gallery__image"
+                                        src="${item.preview}"
+                                        data-source="${item.original}"
+                                        alt="${item.description}"
+                                    />
+                                </a>
+                            </li>`)
+                        .join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener("click", onClickGallery)
+
+function onClickGallery(event) {
+    event.preventDefault();
+        if(event.target.nodeName !== "IMG"){
+            return;
+        }
+
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
+    
+    instance.show();
+
+    document.addEventListener("keydown", onClose);
+
+    function onClose(event) {
+        if (event.code === "Escape") {
+            instance.close();
+            document.removeEventListener("keyup", onClose);
+        }
+    }
+};
+
+
 console.log(galleryItems);
+
+
+
+
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+/*Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+*/
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+                        .map((item) => 
+                            `<li class="gallery__item">
+                                <a class="gallery__link" href="${item.original}">
+                                    <img class="gallery__image" 
+                                    src="${item.preview}"
+                                    alt="${item.description}" />
+                                </a>
+                         </li>`)
+                        .join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+
+galleryContainer.addEventListener("click", onClickGallery)
+
+function onClickGallery(event) {
+    event.preventDefault();
+        if(event.target.nodeName !== "IMG"){
+            return;
+        }
+
+    const lightboxGallery = new SimpleLightbox('.gallery a', {
+        captions: true, // додати підписи до зображень
+        captionsData: 'alt', // брати текст підписів з атрибуту alt
+        captionDelay: 250, // затримка перед появою підписів
+        animationSpeed: 250,
+    });
+};
+
 console.log(galleryItems);
